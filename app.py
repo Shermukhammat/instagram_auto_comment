97ddee5eff8213c8f47e05039126b340bd705add
Shermukhammat <shermukhammadtemirov@gmail.com>
2024-09-12 17:57:28 +0500

    some file types photos and database has been added to git ingnor file

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import uvicorn
 
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="127.0.0.1", port=5000)
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=5000)
