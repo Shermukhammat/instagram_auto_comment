fde72265960728f8cc492a97f998f4c5a5bf1766
SHermukhammad <shermukhammadtemirov@gmail.com>
2024-10-13 06:59:00 -0700

    writng login to file added

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from loader import app
-import handlers, uvicorn 
+import handlers, uvicorn, logging
 
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("data/logfile.log"),
+        # logging.StreamHandler()  # Still prints logs to terminal
+    ]
+)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, log_config=None)
